490a1b2b03826c472829858399f14ebd26bfb239
Amogha V A <amogha.va.iyer@gmail.com>
2025-03-20 21:57:24 +0530

    Started Adding feature to add cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,7 +4,32 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
     console.error("error");
-    throw new error("cabins couldnt be loaded");
+    throw new Error("cabins couldnt be loaded");
   }
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    // console.error("error");
+    throw new Error("cabins couldnt be created");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error("error");
+    throw new Error("cabins couldnt be deleted");
+  }
+
   return data;
 }
